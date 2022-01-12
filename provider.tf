705d612d81459b8b8bb189745d6da4a69e7130ff
Anh Luu <78747045+AnhLuu123@users.noreply.github.com>
2022-01-12 17:34:37 +0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    access_key = "AKIAUQA7DY67KBN3ZAOM"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
     region = "ap-southeast-1"  
 }
